261042d70abb671349688ddbc16d1c1ae50813b2
joud-alnsour <alnsour.joud@gmail.com>
2022-02-13 21:44:14 +0200

    done

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,13 @@
 import Header from '../header/Header';
 import Tours from '../tours/Tours';
 import Footer from '../footer/Footer';
+import data from '../../data/db'
 function Home() {
 
     return (
       <>
         <div><Header/></div>
-        <div><Tours/></div>
+        <div><Tours data={data}/></div>
         <div><Footer/></div>
       </>
     )
